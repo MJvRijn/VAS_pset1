70bb8d0d101cdcea0f43676ae31b7b09f95e74ba
Matthew van Rijn <matthew.van.rijn@gmail.com>
2016-09-06 18:04:54 +0200

    Implemented potato persistence through restarts and orientation changes

--- app/src/main/java/nl/mjvrijn/matthewvanrijn_pset1/PotatoActivity.java
+++ app/src/main/java/nl/mjvrijn/matthewvanrijn_pset1/PotatoActivity.java
@@ -1,58 +1,70 @@
 package nl.mjvrijn.matthewvanrijn_pset1;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class PotatoActivity extends AppCompatActivity {
+
+    SharedPreferences preferences;
+    Map<View, View> map = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_potato);
 
+        map.put(findViewById(R.id.checkbox_arms), findViewById(R.id.potato_arms));
+        map.put(findViewById(R.id.checkbox_ears), findViewById(R.id.potato_ears));
+        map.put(findViewById(R.id.checkbox_eyebrows), findViewById(R.id.potato_eyebrows));
+        map.put(findViewById(R.id.checkbox_eyes), findViewById(R.id.potato_eyes));
+        map.put(findViewById(R.id.checkbox_glasses), findViewById(R.id.potato_glasses));
+        map.put(findViewById(R.id.checkbox_hat), findViewById(R.id.potato_hat));
+        map.put(findViewById(R.id.checkbox_mouth), findViewById(R.id.potato_mouth));
+        map.put(findViewById(R.id.checkbox_mustache), findViewById(R.id.potato_mustache));
+        map.put(findViewById(R.id.checkbox_nose), findViewById(R.id.potato_nose));
+        map.put(findViewById(R.id.checkbox_shoes), findViewById(R.id.potato_shoes));
+
+        preferences = getSharedPreferences("PotatoState", Context.MODE_PRIVATE);
+
+        for(Map.Entry<View,View> pair : map.entrySet()) {
+            CheckBox checkbox = (CheckBox) pair.getKey();
+            View bodypart = pair.getValue();
+
+            boolean visible = preferences.getBoolean((String) checkbox.getText(), false);
+
+            if(visible) {
+                bodypart.setVisibility(View.VISIBLE);
+                checkbox.setChecked(true);
+            } else {
+                checkbox.setChecked(false);
+            }
+        }
     }
 
     public void toggleImage(View view) {
-        switch(view.getId()) {
-            case R.id.checkbox_arms:
-                toggleVisibility(findViewById(R.id.potato_arms));
-                break;
-            case R.id.checkbox_ears:
-                toggleVisibility(findViewById(R.id.potato_ears));
-                break;
-            case R.id.checkbox_eyebrows:
-                toggleVisibility(findViewById(R.id.potato_eyebrows));
-                break;
-            case R.id.checkbox_eyes:
-                toggleVisibility(findViewById(R.id.potato_eyes));
-                break;
-            case R.id.checkbox_glasses:
-                toggleVisibility(findViewById(R.id.potato_glasses));
-                break;
-            case R.id.checkbox_hat:
-                toggleVisibility(findViewById(R.id.potato_hat));
-                break;
-            case R.id.checkbox_mouth:
-                toggleVisibility(findViewById(R.id.potato_mouth));
-                break;
-            case R.id.checkbox_mustache:
-                toggleVisibility(findViewById(R.id.potato_mustache));
-                break;
-            case R.id.checkbox_nose:
-                toggleVisibility(findViewById(R.id.potato_nose));
-                break;
-            case R.id.checkbox_shoes:
-                toggleVisibility(findViewById(R.id.potato_shoes));
-                break;
-        }
-    }
+        String name = (String) ((CheckBox) view).getText();
+        View bodypart = map.get(view);
 
-    private void toggleVisibility(View view) {
-        if(view.getVisibility() == View.VISIBLE) {
-            view.setVisibility(View.INVISIBLE);
+        SharedPreferences.Editor editor = preferences.edit();
+
+        boolean visible = preferences.getBoolean(name, false);
+
+        if(visible) {
+            bodypart.setVisibility(View.INVISIBLE);
+            editor.putBoolean(name, false);
         } else {
-            view.setVisibility(View.VISIBLE);
+            bodypart.setVisibility(View.VISIBLE);
+            editor.putBoolean(name, true);
         }
+
+        editor.apply();
+
     }
 }
