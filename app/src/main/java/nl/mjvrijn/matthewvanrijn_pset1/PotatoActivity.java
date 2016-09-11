aab457d02d4fa57d69221e8093eca7d77e030d2f
Matthew van Rijn <matthew.van.rijn@gmail.com>
2016-09-11 15:21:32 +0200

    Improved layout slightly for tablets (not optimised) and documented code

--- app/src/main/java/nl/mjvrijn/matthewvanrijn_pset1/PotatoActivity.java
+++ app/src/main/java/nl/mjvrijn/matthewvanrijn_pset1/PotatoActivity.java
@@ -15,11 +15,18 @@ public class PotatoActivity extends AppCompatActivity {
     SharedPreferences preferences;
     Map<View, View> map = new HashMap<>();
 
+    /* Set the checkboxes and images to their correct states when the app is opened or the phone is
+     * rotated. This uses Android's SharedPreferences, as described in part 17 of Derek Banas'
+     * Youtube tutorial series linked on the course website (https://www.youtube.com/watch?v=1DOeLy26hOE).
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_potato);
 
+        /* The relationship between the checkboxes and the images are mapped in a map. Here
+         * this map is initialised.
+         */
         map.put(findViewById(R.id.checkbox_arms), findViewById(R.id.potato_arms));
         map.put(findViewById(R.id.checkbox_ears), findViewById(R.id.potato_ears));
         map.put(findViewById(R.id.checkbox_eyebrows), findViewById(R.id.potato_eyebrows));
@@ -31,13 +38,19 @@ public class PotatoActivity extends AppCompatActivity {
         map.put(findViewById(R.id.checkbox_nose), findViewById(R.id.potato_nose));
         map.put(findViewById(R.id.checkbox_shoes), findViewById(R.id.potato_shoes));
 
+        /* Whether a body part should be visible is stored as a shared preference. When the app is
+         * opened/rotated it is necessary to iterate through all these preferences to set the images
+         * and checkboxes to the correct state. Since we have a map containing all these pairs we
+         * iterate through all entries in the map and read the preference for that body part.
+         */
         preferences = getSharedPreferences("PotatoState", Context.MODE_PRIVATE);
 
         for(Map.Entry<View,View> pair : map.entrySet()) {
             CheckBox checkbox = (CheckBox) pair.getKey();
             View bodypart = pair.getValue();
+            String name = (String) checkbox.getText(); // Preference key is the body part's name.
 
-            boolean visible = preferences.getBoolean((String) checkbox.getText(), false);
+            boolean visible = preferences.getBoolean(name, false);
 
             if(visible) {
                 bodypart.setVisibility(View.VISIBLE);
@@ -48,6 +61,7 @@ public class PotatoActivity extends AppCompatActivity {
         }
     }
 
+    /* Toggle the visibility of a given body part and update the value of the preference. */
     public void toggleImage(View view) {
         String name = (String) ((CheckBox) view).getText();
         View bodypart = map.get(view);
@@ -65,6 +79,5 @@ public class PotatoActivity extends AppCompatActivity {
         }
 
         editor.apply();
-
     }
 }
